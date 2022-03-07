3b2e11ecb4d5cfaa4cef05b9392b263c59b8cc72
Țuca Emanuel <59021794+tucaEmanuel18@users.noreply.github.com>
2022-03-07 20:34:04 +0200

    Implementing the exchange sub-protocol (#4)

--- merchant.py
+++ merchant.py
@@ -1,25 +1,35 @@
 import socket
-from network_utils import Messenger, Authenticator, KEY_LENGTH
+from network_utils import Messenger, Authenticator, AuthenticationFailedException, KEY_LENGTH, get_uniq_id
 from Crypto.PublicKey import RSA
 import json
-import uuid
+
+PG_ADDRESS = '127.0.0.1'
+PG_PORT = 5003
 
 PORT = 5002
-HOST = '127.0.0.1'
 
 
-def generate_rsa_keys():
-    key = RSA.generate(KEY_LENGTH)
-    f = open('rsa_keys/merchant_key.pem', 'wb')
-    f.write(key.export_key('PEM'))
-    f.close()
+def get_pg_public_key():
+    f = open('rsa_keys/pg_public_key.pem', 'r')
+    return RSA.import_key(f.read())
 
-    public_key = key.public_key()
-    f = open('rsa_keys/merchant_public_key.pem', 'wb')
-    f.write(public_key.export_key('PEM'))
-    f.close()
+def get_my_rsa_keys():
+    f = open('rsa_keys/merchant_key.pem', 'r')
+    key = RSA.import_key(f.read())
+
+    f = open('rsa_keys/merchant_public_key.pem', 'r')
+    public_key = RSA.import_key(f.read())
 
     return key, public_key
+
+# def generate_rsa_keys():
+#     key = RSA.generate(KEY_LENGTH)
+#     public_key = key.public_key()
+#     f = open('rsa_keys/merchant_public_key.pem', 'wb')
+#     f.write(public_key.export_key('PEM'))
+#     f.close()
+#
+#     return key, public_key
 
 
 def generate_wrong_rsa_keys():
@@ -35,37 +45,93 @@ def generate_wrong_rsa_keys():
 
 
 def prepare_sid_message():
-    sid = str(uuid.uuid4())
-    signed_sid = authenticator.sign(sid)
+    sid = get_uniq_id()
     msg = {
         "sid": sid,
-        "signature": signed_sid
+        "signature": authenticator.sign(sid)
     }
     return sid, json.dumps(msg)
 
 
+def setup_sub_protocol():
+    client_public_key = RSA.import_key(client_messenger.receive().encode())
+    print("LOG: Received the client public RSA key")
+    client_messenger.set_dest_pub_key(client_public_key)
+
+    current_sid, sid_message = prepare_sid_message()
+    client_messenger.send(sid_message)
+    print("LOG: Sent the sid to the client")
+
+    clients_registry[current_sid] = client_public_key
+
+
+def exchange_sub_protocol():
+    def authenticate_po():
+        signature = po.pop("signature")  # remove the 'signature' key to obtain the signed part of the message
+        if authenticator.verify(json.dumps(po), signature, clients_registry[po['sid']]):
+            print(f"LOG: The received message is authentic!")
+        else:
+            raise AuthenticationFailedException()
+
+    def pg_request():
+        def get_msg_to_pg():
+            client_pub_key = clients_registry[po['sid']]
+            msg_for_signature = json.dumps({
+                'sid': po['sid'],
+                'pub_key_c': client_pub_key.export_key('PEM').decode(),
+                'amount': po['amount']
+            })
+            return json.dumps({
+                "pm": pm,
+                "signature": authenticator.sign(msg_for_signature)
+            })
+
+        pg_messenger = Messenger(private_key)
+        pg_messenger.set_dest_pub_key(get_pg_public_key())
+        pg_socket = socket.socket()
+        print("LOG: PG Socket successfully created!")
+        pg_socket.connect((PG_ADDRESS, PG_PORT))
+        print("LOG: Successfully connected with the payment gateway socket!")
+        pg_messenger.set_channel(pg_socket)
+
+        pg_messenger.send(get_msg_to_pg())
+        print("LOG: The transaction request is sent to the payment gateway!")
+        return pg_messenger.receive()
+
+    msg_from_client = json.loads(client_messenger.receive())
+    pm = msg_from_client["pm"]
+    po = msg_from_client["po"]
+    if po['sid'] in clients_registry:
+        authenticate_po()
+        pg_response = pg_request()
+        print(f"LOG: I receive the pg respone: '{pg_response}'")
+        client_messenger.send(pg_response)
+        print("LOG: The transaction response is sent to the client!")
+    else:
+        # @TODO  To return specific message to the client!
+        print("ERROR: Invalid SID")
+        return False
+
+
 if __name__ == '__main__':
-    private_key, public_key = generate_rsa_keys()
+    clients_registry = {}
+    private_key, public_key = get_my_rsa_keys()
+    client_messenger = Messenger(private_key)
     authenticator = Authenticator(private_key)
 
-    s = socket.socket()
+    client_socket = socket.socket()
     print("LOG: Socket successfully created")
-    s.bind(('', PORT))
-    print("LOG: Socket binded to %s" % (PORT))
-    s.listen(5)
+    client_socket.bind(('', PORT))
+    print("LOG: Socket binded to %s" % PORT)
+    client_socket.listen(5)
     print("LOG: Socket is listening")
 
     while True:
-        c, addr = s.accept()
+        client_channel, addr = client_socket.accept()
         print('LOG: Got connection from', addr)
-        client_messenger = Messenger(private_key, c)
+        client_messenger.set_channel(client_channel)
 
-        # Step1 - Setup Sub-protocol
-        client_public_key = RSA.import_key(client_messenger.receive().encode())
-        print("LOG: Received the client public RSA key")
-        client_messenger.set_dest_pub_key(client_public_key)
+        setup_sub_protocol()
+        exchange_sub_protocol()
 
-        current_sid, sid_message = prepare_sid_message()
-        client_messenger.send(sid_message)
-        print("LOG: Sent the sid to the client")
-        c.close()
+    client_socket.close()
--- payment_gateway.py
+++ payment_gateway.py
@@ -1,10 +1,128 @@
 import json
+from Crypto.PublicKey import RSA
+import socket
 
-if __name__ == '__main__':
+from client import KEY_LENGTH
+from network_utils import Messenger, Authenticator, Packer, AuthenticationFailedException
+
+PORT = 5003
+
+
+def get_data():
     f = open('dummy_card_data.json')
     jsonData = json.load(f)
     print(jsonData)
     for i in jsonData['data']['cards']:
         print(i)
-
     f.close()
+
+    # return both merchants and cards dictionaries
+
+
+def get_merchant_public_key():
+    f = open('rsa_keys/merchant_public_key.pem', 'r')
+    return RSA.import_key(f.read())
+
+
+def get_my_rsa_keys():
+    f = open('rsa_keys/pg_key.pem', 'r')
+    key = RSA.import_key(f.read())
+
+    f = open('rsa_keys/pg_public_key.pem', 'r')
+    public_key = RSA.import_key(f.read())
+
+    return key, public_key
+
+
+# def generate_rsa_keys():
+#     key = RSA.generate(KEY_LENGTH)
+#     public_key = key.public_key()
+#     f = open('rsa_keys/pg_public_key.pem', 'wb')
+#     f.write(public_key.export_key('PEM'))
+#     f.close()
+#
+#     return key, public_key
+
+def authenticate_messages(msg):
+    def auth_merchant():
+        message_for_auth = json.dumps({
+            'sid': pm['sid'],
+            'pub_key_c': pm['public_key'],
+            'amount': pm['amount']
+        })
+
+        if authenticator.verify(message_for_auth, msg['signature'], merchant_key):
+            print(f"LOG: The received message from merchant is authentic!")
+        else:
+            raise AuthenticationFailedException()
+
+    def auth_client():
+        signature = pm.pop('signature')
+        client_pub_key = RSA.import_key(pm['public_key'].encode())
+        if authenticator.verify(json.dumps(pm), signature, client_pub_key):
+            print(f"LOG: The received message from client through merchant is authentic!")
+        else:
+            raise AuthenticationFailedException()
+
+    auth_merchant()
+    auth_client()
+
+
+def resolve_transaction(pm):
+    def get_transaction_resp():
+        return "The transaction was successful"
+
+    resp = get_transaction_resp()
+    msg_for_sign = {
+        "resp": resp,
+        "sid": pm['sid'],
+        "amount": pm['amount'],
+        "nc": pm['nc']
+    }
+
+    return json.dumps(
+        {
+            "resp": get_transaction_resp(),
+            "sid": pm['sid'],
+            "signature": authenticator.sign(json.dumps(msg_for_sign))
+        }
+    )
+
+
+if __name__ == '__main__':
+    private_key, public_key = get_my_rsa_keys()
+    merchant_key = get_merchant_public_key()
+    messenger = Messenger(private_key)
+    authenticator = Authenticator(private_key)
+    packer = Packer()
+
+    client_socket = socket.socket()
+    print("LOG: Socket successfully created")
+    client_socket.bind(('', PORT))
+    print("LOG: Socket binded to %s" % PORT)
+    client_socket.listen(5)
+    print("LOG: Socket is listening")
+
+    while True:
+        channel, addr = client_socket.accept()
+        print('LOG: Got connection from', addr)
+        messenger.set_channel(channel)
+        msg = json.loads(messenger.receive())
+
+        if 'pm' in msg:  # it is a merchant
+            print("LOG: Its a transaction request from a merchant")
+            messenger.set_dest_pub_key(merchant_key)
+
+            encrypted_pm = msg['pm']
+            pm = json.loads(
+                messenger.decrypt(
+                    packer.unpack(encrypted_pm['msg']),
+                    packer.unpack(encrypted_pm['key'])
+                )
+            )
+            authenticate_messages(msg)
+            messenger.send(resolve_transaction(pm))
+        else:
+            client_public_key = RSA.import_key(msg['client_pub_key'].encode())
+            messenger.set_dest_pub_key(client_public_key)
+            messenger.send("Resolution Flow - To Be implemented!")
